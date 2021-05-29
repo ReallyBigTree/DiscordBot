31b689712854032b62d4cfcb44e45dcdbf1a1f3e
ReallyBigTree <TreeAnon@protonmail.com>
2021-05-29 15:54:35 -0400

    added help menu

--- bot.py
+++ bot.py
@@ -1,10 +1,34 @@
 import discord
 from discord.ext import commands, tasks
+import pyowm
+import glob
+import ffmpeg
+import asyncio
 
-botClient= commands.Bot(command_prefix='.')
 
+#Initializes bot with command prefix.
+botClient= commands.Bot(command_prefix='!')
+
+#OpenWeatherMap key
+owm = pyowm.OWM('384117676beab9667270502d1e668946')
+
+#Writes Status to console
 @botClient.event
 async def on_ready():
     print('The Discord bot is ready')
 
+#Help Command
+@botClient.command()
+async def bighelp(ctx):
+    await ctx.send(
+        ' !ping\n'
+        )
+
+    
+    
+    
+    
+    
+    
+    
 botClient.run('ODQ4MjMxMjUzNzc3MzE3OTA4.YLJm6g.92A0MZxnB-lHiZR6yZeb5YePX-k')
