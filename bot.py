4ab4ee463f07f872091f77b08ad5bd23144b96e1
ReallyBigTree <TreeAnon@protonmail.com>
2021-05-29 15:57:41 -0400

    added music player feature

--- bot.py
+++ bot.py
@@ -39,6 +39,21 @@ async def bigweather(ctx, *, place):
     observation = weathermgr.weather_at_place(f'{place},us')
     weather = observation.weather
     await ctx.send(f'status: {weather.status}\nTemp: {weather.temp}\nHumidity: {weather.humidity}\n')
+
+#Command to play mp3 from file
+@botClient.command()
+async def music(ctx,*, push):
+    channel=botClient.get_channel(771592178706415616)
+    if push=="play":
+        vchan= await channel.connect()
+        songs = glob.glob('music\*.mp3')
+        for song in songs:
+            vchan.play(discord.FFmpegPCMAudio(f'{song}'))
+            while vchan.is_playing():
+                await asyncio.sleep(1)
+    if push=="stop":
+        for x in botClient.voice_clients:
+            await x.disconnect(
     
     
     
