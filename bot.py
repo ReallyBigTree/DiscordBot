b68ed249ab7beca8dd996dbc2566a0fd30f4d6e4
ReallyBigTree <TreeAnon@protonmail.com>
2021-06-13 15:25:28 -0400

    Added new command that returns a table of the market's largest gainers of the current day

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ import pyowm
 import glob
 import ffmpeg
 import asyncio
-from yahoo_fin import stock_info as stockInfo
+from yahoo_fin import stock_info as stockInfo, stock_info
+from yahoo_finance import Share as shareInfo
+
 
 
 
@@ -75,6 +77,12 @@ async def rock_paper_scissors(ctx, *, userPick):
 async def quote(ctx, *, symbol): 
     getQuote = stockInfo.get_live_price(symbol)
     await ctx.send(symbol + "\n$" + str(round(getQuote, 2)))
+    
+@botClient.command()
+async def gainers(ctx):
+    dailyGainer = stockInfo.get_day_gainers()
+    await ctx.send(dailyGainer)
+    
     
     
 botClient.run('ODQ4MjMxMjUzNzc3MzE3OTA4.YLJm6g.92A0MZxnB-lHiZR6yZeb5YePX-k')
